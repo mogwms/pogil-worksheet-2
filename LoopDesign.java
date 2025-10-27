eaf80f7e7b34d5a786218fe38d525fafe5382cc9
mogwms <mogwms@gmail.com>
2025-10-27 18:46:46 -0400

    "update"

---
+++ LoopDesign.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class LoopDesign {
+    public static Scanner input = new Scanner(System.in);
+    public static Random random = new Random();
+    public static void main(String[] args) {
+        int rollAmount;
+        int timesRolled = 0;
+        int result = 0;
+        
+        System.out.println("Enter how many times to roll the dice:");
+        rollAmount = input.nextInt();
+
+        while (timesRolled < rollAmount) {
+            timesRolled++;
+            result = random.nextInt(6);
+        }
+         System.out.println("The die has been rolled " + rollAmount + " times, and the side landed on is " + result +"!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# pogil-worksheet-2
+# Project Reflection
 October 27, 2025 - POGIL Worksheet 2
+
+Learned how to use the Random class in Java as well as while loops.
